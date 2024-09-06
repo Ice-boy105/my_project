db6a9be7f08239e32e099ee21de6154851317237
Ice-boy105 <ekpeilouis90@gmail.com>
2024-09-06 15:59:58 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// toggle icon navbar /////////////////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
